44ce9fb275d81257cbb0fba38be5464e158f76c2
Augusto Zanoni <augustorz@tutanota.com>
2021-08-09 20:03:19 -0300

    refactor: use data argument

--- index.js
+++ index.js
@@ -6,11 +6,10 @@ AFRAME.registerShader('transparent-video', {
   init: function (data) {
     const videoTexture = new THREE.VideoTexture(data.src);
     videoTexture.format = THREE.RGBAFormat;
+    data.transparent = true;
 
     this.material = new THREE.MeshBasicMaterial({
       map: videoTexture, transparent: true,
     });
-    
-    this.el.components.material.data.transparent = true;
   },
 });
